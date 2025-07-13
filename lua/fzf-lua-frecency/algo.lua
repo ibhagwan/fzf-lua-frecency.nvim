81eefb2a4866461c28d4f7ce9af8b879ce3b1c18
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-07-12 22:08:32 -0400

    rename variables

--- lua/fzf-lua-frecency/algo.lua
+++ lua/fzf-lua-frecency/algo.lua
@@ -29,61 +29,68 @@ M.add = function(filename, opts)
     h.notify_debug(("-"):rep(7 + #filename))
   end
 
-  local scored_files_path = vim.fs.joinpath(db_dir, "scored-files.mpack")
-  local sorted_files_path = vim.fs.joinpath(db_dir, "sorted-files.mpack")
+  local dated_files_path = vim.fs.joinpath(db_dir, "dated-files.mpack")
+  local sorted_scored_files_path = vim.fs.joinpath(db_dir, "sorted-scored-files.mpack")
 
   local now = os.time()
 
-  local scored_files = fs.read(scored_files_path)
-  if not scored_files[cwd] then
-    scored_files[cwd] = {}
+  local dated_files = fs.read(dated_files_path)
+  if not dated_files[cwd] then
+    dated_files[cwd] = {}
   end
 
   local score = 0
-  local date_at_one_point = scored_files[cwd][filename]
-  if date_at_one_point then
-    score = math.exp(DECAY_RATE * (date_at_one_point - now))
+  local date_at_score_one = dated_files[cwd][filename]
+  if date_at_score_one then
+    score = math.exp(DECAY_RATE * (date_at_score_one - now))
   end
   local updated_score = score + 1
-  local updated_date_at_one_point = now + math.log(updated_score) / DECAY_RATE
+  local updated_date_at_score_one = now + math.log(updated_score) / DECAY_RATE
 
-  scored_files[cwd][filename] = updated_date_at_one_point
-  fs.write(scored_files_path, scored_files)
+  dated_files[cwd][filename] = updated_date_at_score_one
+  fs.write(dated_files_path, dated_files)
 
-  local scored_cwd_list = {}
-  for entry_file, entry_date_at_one_point in pairs(scored_files[cwd]) do
+  local scored_files = {}
+  for entry_file, entry_date_at_one_point in pairs(dated_files[cwd]) do
     local recomputed_score = math.exp(DECAY_RATE * (entry_date_at_one_point - now))
-    table.insert(scored_cwd_list, { filename = entry_file, score = recomputed_score, })
+    table.insert(scored_files, { filename = entry_file, score = recomputed_score, })
   end
 
   if debug then
     h.notify_debug("now: %s", M._get_pretty_date(now))
-    h.notify_debug("scored_files: %s", vim.inspect(scored_files))
+    h.notify_debug("dated_files: %s", vim.inspect(dated_files))
     h.notify_debug(
-      "date_at_one_point: %s",
-      date_at_one_point and M._get_pretty_date(date_at_one_point) or "no date_at_one_point"
+      "date_at_score_one: %s",
+      date_at_score_one and M._get_pretty_date(date_at_score_one) or "no date_at_score_one"
     )
     h.notify_debug("score: %s", score)
     h.notify_debug("updated_score: %s", updated_score)
-    h.notify_debug("updated_date_at_one_point: %s", updated_date_at_one_point)
-    h.notify_debug("scored_cwd_list before sort: %s", vim.inspect(scored_cwd_list))
+    h.notify_debug("updated_date_at_score_one: %s", M._get_pretty_date(updated_date_at_score_one))
+    h.notify_debug("scored_files before sort: %s", vim.inspect(scored_files))
   end
 
-  local sorted_files = fs.read(sorted_files_path)
-  if not sorted_files[cwd] then
-    sorted_files[cwd] = {}
+  local sorted_scored_files = fs.read(sorted_scored_files_path)
+  if not sorted_scored_files[cwd] then
+    sorted_scored_files[cwd] = {}
   end
 
-  table.sort(scored_cwd_list, function(a, b)
+  table.sort(scored_files, function(a, b)
     return a.score > b.score
   end)
 
   if debug then
-    h.notify_debug("scored_cwd_list after sort: %s", vim.inspect(scored_cwd_list))
+    h.notify_debug("scored_files after sort: %s", vim.inspect(scored_files))
   end
 
-  sorted_files[cwd] = scored_cwd_list
-  fs.write(sorted_files_path, sorted_files)
+  sorted_scored_files[cwd] = scored_files
+  fs.write(sorted_scored_files_path, sorted_scored_files)
 end
+
+M.add(
+  "/file/alpha",
+  { debug = true, })
+M.add(
+  "/file/beta",
+  { debug = true, })
 
 return M
