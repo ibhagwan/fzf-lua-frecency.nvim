ibhagwan/fzf-lua-frecency.nvim

commit ed4160c3b26b5b7bb17b2ca3e15a021d4eaeb081

format scores, trimming decimals where necessary, misc polish

lua/fzf-lua-frecency/init.lua
@@ -1,7 +1,7 @@
 local h = require "fzf-lua-frecency.helpers"
 local algo = require "fzf-lua-frecency.algo"
 
--- Runtime path for this package, to be used with the headless instance for loading
+-- runtime path for this package, to be used with the headless instance for loading
 local __FILE__ = debug.getinfo(1, "S").source:gsub("^@", "")
 local __RTP__ = vim.fn.fnamemodify(__FILE__, ":h:h:h")
 
@@ -15,26 +15,30 @@ local M = {}
 --- @field display_score boolean
 --- @field [string] any any fzf-lua option
 
-
 local function get_files_cmd(opts)
+  -- https://github.com/ibhagwan/fzf-lua/blob/e40e2337611fa426b8bcb6989fc310035c6ec4aa/README.md?plain=1#L831-L833
+  local default_fd_opts = [[--color=never --hidden --type f --type l --exclude .git]]
+  local default_find_opts = [[-type f \! -path '*/.git/*']]
+  local default_rg_opts = [[--color=never --hidden --files -g "!.git"]]
+
+  local fd_opts = h.default(opts.fd_opts, default_fd_opts)
+  local find_opts = h.default(opts.find_opts, default_find_opts)
+  local rg_opts = h.default(opts.rg_opts, default_rg_opts)
+
   local cmd
-  local fd_args = table.concat({
-    "--absolute-path",
-    "--type", "f",
-    "--type", "l",
-    "--exclude", ".git",
-  }, " ")
-  if vim.fn.executable "fdfind" == 1 then
-    cmd = string.format("fdfind %s", fd_args)
-  elseif vim.fn.executable "fd" == 1 then
-    cmd = string.format("fd %s", fd_args)
-  elseif vim.fn.executable "rg" == 1 then
-    -- return [[rg --files -g "\!.git" "$(pwd)"]]
-    cmd = string.format([[rg --files -g "!.git"]], opts.cmd or vim.uv.cwd())
+  if vim.fn.executable "fdfind" == h.vimscript_true then
+    cmd = ("fdfind %s"):format(fd_opts)
+  elseif vim.fn.executable "fd" == h.vimscript_true then
+    cmd = ("fd %s"):format(fd_opts)
+  elseif vim.fn.executable "rg" == h.vimscript_true then
+    cmd = ("rg %s"):format(rg_opts)
+  elseif vim.fn.executable "find" == h.vimscript_true then
+    cmd = ("find %s"):format(find_opts)
   else
-    FzfLua.utils.warn "[fzf-lua-frecency] 'all_files' requires 'fd' or 'rg'."
+    FzfLua.utils.warn "[fzf-lua-frecency] 'all_files' requires 'fd', 'rg', or 'find'."
     return nil
   end
+
   for k, v in pairs {
     follow = opts.toggle_follow_flag or "-L",
     hidden = opts.toggle_hidden_flag or "--hidden",
@@ -61,23 +65,21 @@ end
 
 --- @diagnostic disable-next-line: unused-local
 M.setup = function(opts)
-  -- Singleton setup
   if M._did_setup then return end
   M._did_setup = true
 
-  -- Trigger lazy loading if need be, creates the FzfLua global object
+  -- creates the FzfLua global object
   require "fzf-lua"
 
-  -- Register as an fzf-lua extenstion, merge default opts with users' setup opts
   FzfLua.register_extension("frecency", M.frecency, vim.tbl_deep_extend("keep", opts or {}, {
       -- fzf-lua-frecency specific defaults
       cwd_only = false,
       all_files = nil,
       stat_file = true,
       display_score = true,
-      -- Relevant options from fzf-lua's default `files` options
-      _type = "file", -- Adds `fn_preprocess` if required
-      previewer = FzfLua.defaults.files.previewer, -- Inherit from default previewer (if `bat`)
+      -- relevant options from fzf-lua's default `files` options
+      _type = "file", -- adds `fn_preprocess` if required
+      previewer = FzfLua.defaults.files.previewer, -- inherit from default previewer (if `bat`)
       multiprocess = true,
       file_icons = true,
       color_icons = true,
@@ -91,11 +93,10 @@ M.setup = function(opts)
         title = " Frecency ",
         preview = { winopts = { cursorline = false, }, },
       },
-      -- Display cwd (if different) and action (ctrl-x) headers
+      -- display cwd (if different) and action (ctrl-x) headers
       _headers = { "cwd", "actions", },
-      -- Inherit actions from the users' setup/global `actions.files`
+      -- inherit actions from the users' setup/global `actions.files`
       _actions = function() return FzfLua.config.globals.actions.files end,
-      -- Adds `ctrl-x` to the default actions
       actions = {
         ["ctrl-x"] = {
           fn = function(selected, o)
@@ -111,37 +112,32 @@ M.setup = function(opts)
       },
     }),
     true)
-  -- Update score of all files when editing / changing windows
+
   vim.api.nvim_create_autocmd({ "BufWinEnter", }, {
     group = vim.api.nvim_create_augroup("FzfLuaFrecency", { clear = true, }),
     callback = function(ev)
       local current_win = vim.api.nvim_get_current_win()
-      if vim.api.nvim_win_get_config(current_win).relative ~= "" then
-        return
+      -- :h nvim_win_get_config({window}) "relative is empty for normal buffers"
+      if vim.api.nvim_win_get_config(current_win).relative == "" then
+        algo.update_file_score(vim.api.nvim_buf_get_name(ev.buf), { update_type = "increase", })
       end
-      algo.update_file_score(vim.api.nvim_buf_get_name(ev.buf), { update_type = "increase", })
     end,
   })
 end
 
 --- @param opts FrecencyFnOpts
 M.frecency = function(opts)
-  -- Does nothing if already called, will lazy load fzf-lua
-  -- and create the FzfLua global object
+  -- does nothing if already called
   M.setup()
 
-  -- Normalize users' opts with our (registered) defaults and fzf-lua's
-  -- (keymaps, previewers, special options, fzf/skim version check, etc)
   opts = FzfLua.config.normalize_opts(opts, "frecency")
   if not opts then return end
 
-  -- Set default cwd if needed
-  opts.cwd = opts.cwd or vim.uv.cwd()
-
+  opts.cwd = h.default(opts.cwd, vim.uv.cwd())
   local db_dir = h.default(opts.db_dir, h.get_default_db_dir())
   local sorted_files_path = h.get_sorted_files_path(db_dir)
 
-  -- Options that fzf-lua's multiprocess does not serialize
+  -- options that fzf-lua's multiprocess does not serialize
   -- these aren't included in the fn_transform callback opts
   --- @type GetFnTransformOpts
   local encodeable_opts = {
@@ -150,16 +146,12 @@ M.frecency = function(opts)
     display_score = opts.display_score,
   }
 
-  -- Clear the global db vars on exit, only matters on `multiprocess=false`
-  -- with `multiprocess=true` fn_preprocess is called in the headless process
-  -- and the global vars are created there
   opts.fn_selected = function(...)
     _G._fzf_lua_frecency_dated_files = nil
-    _G._fzf_lua_frecency_max_scores = nil
     FzfLua.actions.act(...)
   end
 
-  -- RPC worked fine on linux, be was hanging on mac - specifically vim.rpcrequest
+  -- RPC worked fine on linux, but was hanging on mac - specifically vim.rpcrequest
   -- using basic string interpolation works well since all the opts that are used
   -- can be stringified
   opts.fn_transform = string.format([[
@@ -168,24 +160,31 @@ M.frecency = function(opts)
     return require "fzf-lua-frecency.fn_transform".get_fn_transform(rpc_opts)
   ]], __RTP__, vim.mpack.encode(encodeable_opts))
 
-  -- Create the shell command, adds file enumeration (fd|rg) and dedup (awk)
   opts.cmd = (function()
-    -- If caller did not specifically set `all_files` set to true if `cwd_only=true`
-    local all_files = opts.all_files == nil and opts.cwd_only or opts.all_files
+    local all_files
+    if opts.all_files == nil then
+      all_files = opts.cwd_only
+    else
+      all_files = false
+    end
+
     local cat_cmd = table.concat({
       "cat",
       sorted_files_path,
-      "2>/dev/null",
+      "2>/dev/null", -- in case the file doesn't exist
     }, " ")
-    if not all_files then return cat_cmd end
+    if not all_files then
+      opts.cmd = cat_cmd
+    end
+
     local all_files_cmd = get_files_cmd(opts)
-    -- `all_files_cmd` will return nil of fd|rg aren't installed
     if not all_files_cmd then return cat_cmd end
+
     local awk_cmd = "awk '!x[$0]++'" -- https://stackoverflow.com/a/11532198
     return ("(%s; %s) | %s"):format(cat_cmd, all_files_cmd, awk_cmd)
   end)()
 
-  -- Set title flags (h|i|f) based on hidden/no-ignore/follow flags
+  -- set title flags (h|i|f) based on hidden/no-ignore/follow flags
   opts = FzfLua.core.set_title_flags(opts, { "cmd", })
   return FzfLua.fzf_exec(opts.cmd, opts)
 end
@@ -193,8 +192,8 @@ end
 --- @class ClearDbOpts
 --- @field db_dir? string
 
---- Deletes the `dated-files.mpack` file and the `cwds` directory.
---- Does not delete `db_dir` itself or anything else in `db_dir`
+--- deletes the `dated-files.mpack` file and the `cwds` directory.
+--- does not delete `db_dir` itself or anything else in `db_dir`
 --- @param opts? ClearDbOpts
 M.clear_db = function(opts)
   opts = opts or {}

lua/fzf-lua-frecency/algo.lua
@@ -69,7 +69,6 @@ M.update_file_score = function(filename, opts)
 
   local sorted_files_path = h.get_sorted_files_path(db_dir)
   local dated_files_path = h.get_dated_files_path(db_dir)
-  local max_scores_path = h.get_max_scores_path(db_dir)
 
   if debug then
     h.notify_debug_header("DEBUG: update_file_score %s", filename)
@@ -118,14 +117,11 @@ M.update_file_score = function(filename, opts)
   --- @type ScoredFile[]
   local scored_files = {}
   local updated_dated_files = {}
-  local max_score = 0
   for dated_file_entry, date_at_one_point_entry in pairs(dated_files[db_index]) do
     local recomputed_score = M.compute_score { now = now, date_at_score_one = date_at_one_point_entry, }
 
     local readable = vim.uv.fs_stat(dated_file_entry) ~= nil
-
     if readable then
-      max_score = math.max(max_score, recomputed_score)
       table.insert(scored_files, { filename = dated_file_entry, score = recomputed_score, })
       updated_dated_files[dated_file_entry] = date_at_one_point_entry
     end
@@ -134,13 +130,6 @@ M.update_file_score = function(filename, opts)
   fs.write {
     data = dated_files,
     path = dated_files_path,
-    encode = true,
-  }
-  local max_scores = fs.read(max_scores_path)
-  max_scores[db_index] = max_score
-  fs.write {
-    data = max_scores,
-    path = max_scores_path,
     encode = true,
   }
 

lua/fzf-lua-frecency/fn_transform.lua
@@ -8,12 +8,9 @@ local M = {}
 --- @param rpc_opts GetFnTransformOpts
 M.get_fn_transform = function(rpc_opts)
   return function(abs_file, opts)
-    -- Call fzf-lua's entry maker, filters out cwd/cwd_only/file_ignore_patterns, etc
-    -- will also make the file path relative, use formatters, path_shorten, etc
     local entry = FzfLua.make_entry.file(abs_file, opts)
     if not entry then return end
 
-    -- If we don't display score or test for the file on disk stop here
     if not rpc_opts.display_score and not rpc_opts.stat_file then
       return entry
     end
@@ -26,23 +23,22 @@ M.get_fn_transform = function(rpc_opts)
 
     if not _G._fzf_lua_frecency_dated_files then
       local dated_files_path = h.get_dated_files_path(rpc_opts.db_dir)
-      local max_scores_path = h.get_max_scores_path(rpc_opts.db_dir)
       _G._fzf_lua_frecency_dated_files = fs.read(dated_files_path)
-      _G._fzf_lua_frecency_max_scores = fs.read(max_scores_path)
-     end
+    end
 
     local dated_files = _G._fzf_lua_frecency_dated_files
-    local max_scores = _G._fzf_lua_frecency_max_scores
-    local max_score = h.default(max_scores[db_index], 0)
+    if not dated_files[db_index] then
+      dated_files[db_index] = {}
+    end
+    local date_at_score_one = dated_files[db_index][abs_file]
+
+    local max_score = 999
     local max_score_len = #h.exact_decimals(max_score, 2)
 
-    local date_at_score_one = dated_files[db_index] and dated_files[db_index][abs_file] or nil
-
-    -- Only "stat" files from the db, fd|rg enumerated files guaranteed to exist
+    -- only call fs_stat on files from the db, fd/rg files are guaranteed to exist
     if rpc_opts.stat_file and date_at_score_one then
       if not vim.uv.fs_stat(abs_file) then
         vim.schedule(function()
-          -- File no longer exists,remove from the db, schedule to avoid E5560
           algo.update_file_score(abs_file, { update_type = "remove", })
         end)
         return
@@ -56,12 +52,10 @@ M.get_fn_transform = function(rpc_opts)
       end
 
       local formatted_score
-      if max_score == 0 then
-        formatted_score = ""
-      elseif score == nil then
+      if score == nil then
         formatted_score = (" "):rep(max_score_len)
       else
-        formatted_score = h.pad_str(h.exact_decimals(score, 2), max_score_len)
+        formatted_score = h.pad_str(h.fit_decimals(score, max_score_len), max_score_len)
       end
       return ("%s%s%s"):format(formatted_score, FzfLua.utils.nbsp, entry)
     end

lua/fzf-lua-frecency/health.lua
@@ -1,3 +1,4 @@
+local h = require "fzf-lua-frecency.helpers"
 local M = {}
 
 function M.check()
@@ -10,11 +11,24 @@ function M.check()
     })
   end
 
-  if vim.fn.executable "fd" == 1 then
+  if vim.fn.executable "fdfind" == h.vimscript_true then
     vim.health.ok "'fd' is installed"
+  elseif vim.fn.executable "fd" == h.vimscript_true then
+    vim.health.ok "'fd' is installed"
+  elseif vim.fn.executable "rg" == h.vimscript_true then
+    vim.health.ok "'rg' is installed"
   else
-    vim.health.error("'fd' is not installed", {
+    vim.health.error("'all_files' requires 'fd' or 'rg' to be installed", {
       "Install fd: https://github.com/sharkdp/fd",
+      "Install rg: https://github.com/BurntSushi/ripgrep",
+    })
+  end
+
+  if vim.fn.executable "awk" == h.vimscript_true then
+    vim.health.ok "'awk' is installed"
+  else
+    vim.health.error("'awk' is not installed", {
+      "On windows, one installation method for 'awk' is through the choco package manager: https://community.chocolatey.org/packages/awk",
     })
   end
 end

lua/fzf-lua-frecency/helpers.lua
@@ -70,6 +70,22 @@ M.exact_decimals = function(num, decimals)
   return M.min_decimals(M.max_decimals(num, decimals), decimals)
 end
 
+M.fit_decimals = function(num, max_len)
+  local two_decimals = M.exact_decimals(num, 2)
+
+  if #two_decimals <= max_len then
+    return two_decimals
+  end
+
+  local one_decimal = M.exact_decimals(tonumber(two_decimals), 1)
+  if #one_decimal <= max_len then
+    return one_decimal
+  end
+
+  local no_decimals = M.exact_decimals(tonumber(two_decimals), 0)
+  return no_decimals
+end
+
 M.get_default_db_dir = function()
   return vim.fs.joinpath(vim.fn.stdpath "data", "fzf-lua-frecency")
 end

tests/test_algo.lua
@@ -7,7 +7,6 @@ local db_dir = vim.fs.joinpath(root_dir, "db-dir")
 local db_index = 1
 local sorted_files_path = h.get_sorted_files_path(db_dir)
 local dated_files_path = h.get_dated_files_path(db_dir)
-local max_scores_path = h.get_max_scores_path(db_dir)
 
 local cwd = vim.fs.joinpath(root_dir, "files")
 local test_file_a = vim.fs.joinpath(cwd, "test-file-a.txt")
@@ -102,7 +101,6 @@ T["#update_file_score"]["update_type=increase"]["adds score entry for new file"]
   local date_at_score_one = dated_files[db_index][test_file_a]
   MiniTest.expect.equality(date_at_score_one, date_at_score_one_now)
   MiniTest.expect.equality(read_sorted(), test_file_a .. "\n")
-  MiniTest.expect.equality(fs.read(max_scores_path)[db_index], score_when_adding)
 end
 
 T["#update_file_score"]["update_type=increase"]["increments score on repeated calls"] = function()
@@ -127,8 +125,6 @@ T["#update_file_score"]["update_type=increase"]["increments score on repeated ca
     fs.read(dated_files_path)[db_index][test_file_a],
     algo.compute_date_at_score_one { now = now_after_30_min, score = score_decayed_after_30_min + 1, }
   )
-  -- TODO: precision issue, values are the same
-  -- MiniTest.expect.equality(fs.read(max_scores_path)[db_index], score_decayed_after_30_min + 1)
 end
 
 T["#update_file_score"]["update_type=increase"]["recalculates all scores when adding a new file"] = function()
@@ -158,7 +154,6 @@ T["#update_file_score"]["update_type=increase"]["recalculates all scores when ad
     algo.compute_date_at_score_one { now = now_after_30_min, score = score_when_adding, }
   )
   MiniTest.expect.equality(read_sorted(), test_file_b .. "\n" .. test_file_a .. "\n")
-  MiniTest.expect.equality(fs.read(max_scores_path)[db_index], 1)
 end
 
 T["#update_file_score"]["update_type=increase"]["filters deleted files"] = function()
@@ -190,7 +185,6 @@ T["#update_file_score"]["update_type=increase"]["filters deleted files"] = funct
     algo.compute_date_at_score_one { now = now_after_30_min, score = score_when_adding, }
   )
   MiniTest.expect.equality(read_sorted(), test_file_b .. "\n")
-  MiniTest.expect.equality(fs.read(max_scores_path)[db_index], 1)
 end
 
 T["#update_file_score"]["update_type=remove"] = MiniTest.new_set()
@@ -203,7 +197,6 @@ T["#update_file_score"]["update_type=remove"]["adds entry for existing file"] = 
 
   MiniTest.expect.equality(fs.read(dated_files_path)[db_index][test_file_a], date_at_score_one_now)
   MiniTest.expect.equality(read_sorted(), test_file_a .. "\n")
-  MiniTest.expect.equality(fs.read(max_scores_path)[db_index], score_when_adding)
 
   algo._now = function() return now end
   algo.update_file_score(test_file_a, {
@@ -213,7 +206,6 @@ T["#update_file_score"]["update_type=remove"]["adds entry for existing file"] = 
 
   MiniTest.expect.equality(fs.read(dated_files_path)[db_index][test_file_a], nil)
   MiniTest.expect.equality(read_sorted(), "")
-  MiniTest.expect.equality(fs.read(max_scores_path)[db_index], 0)
 end
 
 return T

tests/test_helpers.lua
@@ -43,6 +43,20 @@ T["#exact_decimals"]["truncates then formats to exact decimals"] = function()
   MiniTest.expect.equality(h.exact_decimals(5, 3), "5.000")
 end
 
+T["#fit_decimals"] = MiniTest.new_set()
+T["#fit_decimals"]["returns two decimals when it fits within max_len"] = function()
+  MiniTest.expect.equality(h.fit_decimals(123.45, 7), "123.45")
+  MiniTest.expect.equality(h.fit_decimals(1.23, 4), "1.23")
+end
+T["#fit_decimals"]["returns one decimal when two decimals are too long but one decimal fits"] = function()
+  MiniTest.expect.equality(h.fit_decimals(123.45, 5), "123.4")
+  MiniTest.expect.equality(h.fit_decimals(12.345, 5), "12.34")
+end
+T["#fit_decimals"]["returns no decimals when two decimals are too long"] = function()
+  MiniTest.expect.equality(h.fit_decimals(12345.67, 5), "12345")
+  MiniTest.expect.equality(h.fit_decimals(1234.56, 5), "1234")
+end
+
 T["#strip_score"] = MiniTest.new_set()
 T["#strip_score"]["removes leading numbers and spaces"] = function()
   MiniTest.expect.equality(h.strip_score "1.23 some/file/path", "some/file/path")

tests/test_init.lua
@@ -31,7 +31,6 @@ T["#frecency"]["builds the correct fzf command and calls fzf_exec"] = function()
   }
 
   fzf_lua.fzf_exec = function(cmd, opts)
-    print("cmd", cmd)
     called.cmd = cmd
     called.opts = opts
   end
@@ -44,7 +43,7 @@ T["#frecency"]["builds the correct fzf command and calls fzf_exec"] = function()
   MiniTest.expect.equality(called.opts.all_files, true)
 
   local cat_cmd = ("cat %s 2>/dev/null"):format(sorted_files_path)
-  local fd_cmd = "fd --absolute-path --type f --type l --exclude .git"
+  local fd_cmd = "fd --color=never --hidden --type f --type l --exclude .git"
   MiniTest.expect.equality(called.cmd, ("(%s; %s) | awk '!x[$0]++'"):format(cat_cmd, fd_cmd))
 end
 

commit 0527e6b509c8322f1fb5d3d73bae692cf16145ef

fix: dedup regression from ed4160c
File enumeration commands should output absolute paths for
awk dedup to work.

feat: use `--nth` to exclude score matching

Scores should not match when typing digits, this also excludes
icons and other fzf-lua special markers (git icons, etc) so we
perform special searches like "^init.lua".

lua/fzf-lua-frecency/init.lua
@@ -18,9 +18,9 @@ local M = {}
 
 local function get_files_cmd(opts)
   -- https://github.com/ibhagwan/fzf-lua/blob/e40e2337611fa426b8bcb6989fc310035c6ec4aa/README.md?plain=1#L831-L833
-  local default_fd_opts = [[--color=never --hidden --type f --type l --exclude .git]]
-  local default_find_opts = [[-type f \! -path '*/.git/*']]
-  local default_rg_opts = [[--color=never --hidden --files -g "!.git"]]
+  local default_fd_opts = [[--absolute-path --color=never --hidden --type f --type l --exclude .git]]
+  local default_rg_opts = string.format([[--color=never --hidden --files -g "!.git" %s]], opts.cwd)
+  local default_find_opts = string.format([[%s -type f \! -path '*/.git/*']], opts.cwd)
 
   local fd_opts = h.default(opts.fd_opts, default_fd_opts)
   local find_opts = h.default(opts.find_opts, default_find_opts)
@@ -106,6 +106,11 @@ M.setup = function(opts)
         title = " Frecency ",
         preview = { winopts = { cursorline = false, }, },
       },
+      -- tell fzf to ignore fuzzy matching anything before the filename
+      -- by adding a "--delimiter=utils.nbsp|--nth=-1.." to fzf_opts
+      -- this avoids matching the score text/icons so we can perform
+      -- searches like "^init.lua"
+      _fzf_nth_devicons = true,
       -- display cwd (if different) and action (ctrl-x) headers
       _headers = { "cwd", "actions", },
       -- inherit actions from the users' setup/global `actions.files`